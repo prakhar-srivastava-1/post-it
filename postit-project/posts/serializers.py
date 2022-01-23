935aea53793828ec6d5d5e2400442f83be07b2f4
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-01-23 22:15:35 +0530

    display votes on posts - update serializers.py

--- postit-project/posts/serializers.py
+++ postit-project/posts/serializers.py
@@ -7,10 +7,16 @@ class PostSerializer(serializers.ModelSerializer):
     # remember poster is a django.contrib.auth.models User object
     poster = serializers.ReadOnlyField(source='poster.username')
     poster_id = serializers.ReadOnlyField(source='poster.id')
+    # SerializerMethodField - data will come from a method
+    votes = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
-        fields = ['id', 'title', 'url', 'poster', 'poster_id', 'created']
+        fields = ['id', 'title', 'url', 'poster', 'poster_id', 'created', 'votes']
+
+    # name of method should be get_<variable>() - get_cote() here
+    def get_votes(self, post):
+        return Vote.objects.filter(post=post).count()
 
 
 class VoteSerializer(serializers.ModelSerializer):
