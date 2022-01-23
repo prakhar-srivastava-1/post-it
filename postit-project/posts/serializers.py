87a2ba1c25aa16dc225de0a1e6e8bf54ca9a461f
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-01-23 20:53:58 +0530

    add logic to add the poster automatically while creating a post

--- postit-project/posts/serializers.py
+++ postit-project/posts/serializers.py
@@ -3,6 +3,11 @@ from .models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    # set fields read-only
+    # remember poster is a django.contrib.auth.models User object
+    poster = serializers.ReadOnlyField(source='poster.username')
+    poster_id = serializers.ReadOnlyField(source='poster.id')
+
     class Meta:
         model = Post
-        fields = ['id', 'title', 'url', 'poster', 'created']
+        fields = ['id', 'title', 'url', 'poster', 'poster_id', 'created']
--- postit-project/posts/views.py
+++ postit-project/posts/views.py
@@ -7,3 +7,8 @@ from .serializers import PostSerializer
 class PostList(generics.ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    # override this built-in function to get user
+    # currently logged in and put him as the poster
+    def perform_create(self, serializer):
+        serializer.save(poster=self.request.user)
