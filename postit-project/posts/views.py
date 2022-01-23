82de5f94d53da06714c5867da927d2bc8f9a7a85
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-01-23 20:58:51 +0530

    ensure only authenticated users have access to add posts via API calls - use permissions class from rest_framework

--- postit-project/posts/views.py
+++ postit-project/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Post
 from .serializers import PostSerializer
 
@@ -7,6 +7,7 @@ from .serializers import PostSerializer
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     # override this built-in function to get user
     # currently logged in and put him as the poster
